4bb906d5b3ebe4871ae66639ec3e049d2237fdd7
chenyanxiong <xiongchenyan@gmail.com>
2014-03-11 15:23:23 -0400

    adhoc unit test

---
+++ AdhocEva/AdhocEvaTest.py
@@ -0,0 +1,23 @@
+'''
+Created on Mar 11, 2014
+unit test
+@author: cx
+'''
+
+
+
+import site
+site.addsitedir('/bos/usr0/cx/local/lib/python2.7/site-packages')
+site.addsitedir('/bos/usr0/cx/cxPylib')
+import sys
+
+from AdhocEva import *
+from cxBase.base import cxConf
+
+
+if 2 != len(sys.argv):
+    print "1 para conf:\nin\nqrel\evadepth\nout"
+    sys.exit()
+    
+AdhocEvaUnitTest(sys.argv[1])
+print "done"
