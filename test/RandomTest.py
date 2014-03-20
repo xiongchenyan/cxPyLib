2fbead1feb454d4793d8e465002ceaab453dd16e
chenyanxiong <xiongchenyan@gmail.com>
2014-03-20 12:18:25 -0400

    test

---
+++ test/RandomTest.py
@@ -0,0 +1,16 @@
+'''
+Created on Mar 20, 2014
+
+@author: cx
+'''
+
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+
+from cxBase.base import *
+import sys
+
+conf = cxConf(sys.argv[1])
+
+print conf.GetConf('in')
