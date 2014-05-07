5e577863bff9d2fe1ea6c40bebf78bcd2d1d0c0f
chenyanxiong <xiongchenyan@gmail.com>
2014-05-06 22:26:08 -0400

    add functions to vector

--- cxBase/Vector.py
+++ cxBase/Vector.py
@@ -10,15 +10,20 @@ class VectorC(object):
     def Init(self):
         self.hDim = {}
         
-    def __init__(self,hDict = {}):
+    def __init__(self,InData = {}):
         self.Init()
-        if {} != hDict:
-            self.hDim = deepcopy(hDict)
+        if {} != InData:
+            if type(InData) == dict:
+                self.hDim = deepcopy(InData)
+            if type(InData) == list:
+                self.hDim = dict(zip(InData,range(len(InData))))
+    
+    
         
         
     def __mul__(self,vb):
         if (type(vb) == float) | (type(vb) == int):
-            res = VectorC(self.hDim)
+            res = deepcopy(self)
             for item in res.hDim:
                 res.hDim[item] *= vb
             return res
@@ -34,12 +39,31 @@ class VectorC(object):
     def __div__(self,value):
         return self.__mul__(1.0/value)
     
+    
+    def __deepcopy__(self,memo):
+        res = VectorC()
+        res.hDim = deepcopy(self.hDim,memo)
+        return res
+    
+    def __add__(self,vB):
+        res = deepcopy(self)
+        for item in vB.hDim:
+            if not item in res.hDim:
+                res.hDim[item] = vB.hDim[item]
+            else:
+                res.hDim[item] += vB.hDim[item]
+        return res
+    
+    
     def Mod(self,level=2):
         score = 0
         for dim,value in self.hDim.items():
             score += math.pow(value,2)
         score = math.pow(score,0.5)
         return score
+    
+    
+    
     
     @staticmethod
     def cosine(vA,vB):
