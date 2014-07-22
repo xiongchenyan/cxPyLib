fdbfce72595699b352dd7e6be8d379d4845f8d58
chenyanxiong <xiongchenyan@gmail.com>
2014-07-22 15:30:10 -0400

    new file:   IndriRelate/RenameCachedRes.py

---
+++ IndriRelate/RenameCachedRes.py
@@ -0,0 +1,31 @@
+'''
+Created on Jul 22, 2014
+rename indir cached res
+@author: cx
+'''
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+
+from IndriRelate.IndriPackedRes import *
+import os
+import sys
+
+if 4 != len(sys.argv):
+    print "3para: query in name + query to rename + cashdir"
+    sys.exit()
+    
+    
+CashDir = sys.argv[3]
+
+hQName = {}
+for line in open(sys.argv[2]):
+    qid,query = line.strip().split('\t')
+    hQName[qid] = query
+    
+for line in open(sys.argv[1]):
+    qid,query = line.strip().split('\t')
+    os.rename(CashDir + '/' + query, CashDir + '/' + hQName[qid])
+    
+
+
