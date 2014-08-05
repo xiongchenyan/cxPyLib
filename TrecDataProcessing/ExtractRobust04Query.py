6c2fffe43d07a7ed102af8fc29e6caa01e329e15
chenyanxiong <xiongchenyan@gmail.com>
2014-08-05 14:54:19 -0400

    deal with not same line q

--- TrecDataProcessing/ExtractRobust04Query.py
+++ TrecDataProcessing/ExtractRobust04Query.py
@@ -13,12 +13,20 @@ if 3 != len(sys.argv):
     
 out = open(sys.argv[2],'w')
 qid = '0'
+flag = False
 for line in open(sys.argv[1]):
     line = line.strip()
     if '<num>' in line:
         qid = line.replace('<num> Number:','').strip()
     if '<title>' in line:
         query = line.replace('<title>','').strip().lower()
-        print >>out, qid + '\t' + query
+        if query == '':
+            flag = True
+            continue
+        else:
+            print >>out, qid + '\t' + query
+    if flag:
+        print >>out, qid + "\t" + line.strip()
+        flag = False
 out.close()
     
