60c277b6791594e90a6d7c349655c0a0e65a46cc
chenyanxiong <xiongchenyan@gmail.com>
2014-08-26 13:55:18 -0400

    update

--- IndriRelate/RenameCachedRes.py
+++ IndriRelate/RenameCachedRes.py
@@ -11,13 +11,13 @@ from IndriRelate.IndriPackedRes import *
 import os
 import sys
 
-if 4 != len(sys.argv):
-    print "3para: query in name + query to rename + cashdir"
+if 5 != len(sys.argv):
+    print "3para: query in name + query to rename + cashdir + name length (101|104)"
     sys.exit()
     
     
 CashDir = sys.argv[3]
-
+NameLen = int(sys.argv[4])
 hQName = {}
 for line in open(sys.argv[2]):
     qid,query = line.strip().split('\t')
@@ -27,7 +27,7 @@ for line in open(sys.argv[1]):
     qid,query = line.strip().split('\t')
     try:
         InName = CashDir + '/' + query
-        InName = InName[:104]
+        InName = InName[:NameLen]
         os.rename(InName, CashDir + '/' + hQName[qid])
     except OSError:
         print "rename [%s] to [%s] failed" %(InName, CashDir + '/' +hQName[qid])
