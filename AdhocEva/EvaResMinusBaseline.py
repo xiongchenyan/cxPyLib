a2b07234523358d6162ed3ac4ba36061650018f5
chenyanxiong <xiongchenyan@gmail.com>
2014-09-09 21:36:44 -0400

    move to here

---
+++ AdhocEva/EvaResMinusBaseline.py
@@ -0,0 +1,33 @@
+'''
+Created on Jun 3, 2014
+evaluation results - base line results
+in: eva file (trec_eval) - baseline
+out: score minus baseline's
+@author: cx
+'''
+
+
+import site
+site.addsitedir('/bos/usr0/cx/cxPylib')
+from AdhocMeasure import *
+
+import sys
+if 4 != len(sys.argv):
+    print "3 para: eva res + base line res + output"
+    sys.exit()
+    
+    
+lBasePerQEva = AdhocMeasureC().ReadPerQEva(sys.argv[2],True)
+lThisPerQEva = AdhocMeasureC().ReadPerQEva(sys.argv[1],True)
+
+
+hBaseRes = dict(lBasePerQEva)
+
+out = open(sys.argv[3],'w')
+
+for qid,Measure in lThisPerQEva:
+    BlMeasure = hBaseRes[qid]
+    Measure -= BlMeasure
+    print >> out, "%s\t%s" %(str(qid),Measure.dumps())
+    
+out.close()
