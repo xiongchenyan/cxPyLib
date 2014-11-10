6e95f4c952e6e4f408048b8bf20922f89a744f65
chenyanxiong <xiongchenyan@gmail.com>
2014-11-09 22:15:25 -0500

    form web track result format

---
+++ IndriRelate/WebTrackResultFromCache.py
@@ -0,0 +1,29 @@
+'''
+Created on Nov 9, 2014 10:12:23 PM
+@author: cx
+
+what I do:
+generate web track format ranking result from CacheDir
+what's my input:
+queries + cache dir
+what's my output:
+a rank in cache dir
+'''
+
+from IndriInferencer import *
+import sys
+
+
+if 3 != len(sys.argv):
+    print "query + cache dir"
+    sys.exit()
+    
+    
+for line in open(sys.argv[1]):
+    qid,query = line.strip().split('\t')
+    lDoc = ReadPackedIndriRes(sys.argv[2] + '/' + query, 1000)
+    for i in range(len(lDoc)):
+        print "%s Q0 %s %d %f base" %(qid,lDoc[i].DocNo,i+1,lDoc[i].score)
+        
+
+
