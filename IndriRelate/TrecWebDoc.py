ecd71ec746421ee84c55a7f88870d1ca6e058601
chenyanxiong <xiongchenyan@gmail.com>
2014-12-17 16:14:11 -0500

    add a line break

--- IndriRelate/TrecWebDoc.py
+++ IndriRelate/TrecWebDoc.py
@@ -29,7 +29,7 @@ class TrecWebDocC(object):
         res += "<DOCNO>%s</DOCNO>\n" %(self.DocNo)
         res += "<DOCHDR>\n%s\n</DOCHDR>\n" %(self.Url)
         for field in self.lField:
-            res += "<%s>\n%s</%s>\n" %(field[0],field[1],field[0])
+            res += "<%s>\n%s\n</%s>\n" %(field[0],field[1],field[0])
         res += "</DOC>"
         return res
     
