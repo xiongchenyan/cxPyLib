f1d0749d0b17e726079d96661d6ca030396714e1
chenyanxiong <xiongchenyan@gmail.com>
2014-12-18 15:25:39 -0500

    support new cache

--- IndriRelate/WebTrackResultFromCache.py
+++ IndriRelate/WebTrackResultFromCache.py
@@ -12,17 +12,27 @@ a rank in cache dir
 import site
 site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
 from IndriRelate.IndriInferencer import *
+from IndriSearch.IndriDocBase import *
+from IndriSearch.IndriSearchRunQuery import *
 import sys
 
 
-if 3 != len(sys.argv):
+if 3 > len(sys.argv):
     print "query + cache dir"
     sys.exit()
     
-    
+CacheVersion = 'old'
+if len(sys.argv) > 3:
+    CacheVersion = sys.argv[3]
+    IndriSearcher = IndriSearchCenterC()
+    IndriSearcher.CacheDir = sys.argv[2]    
+
 for line in open(sys.argv[1]):
     qid,query = line.strip().split('\t')
-    lDoc = ReadPackedIndriRes(sys.argv[2] + '/' + query, 1000)
+    if CacheVersion == 'old':
+        lDoc = ReadPackedIndriRes(sys.argv[2] + '/' + query, 1000)
+    else:
+        lDoc = IndriSearcher.RunQuery(query)
     for i in range(len(lDoc)):
         print "%s Q0 %s %d %f base" %(qid,lDoc[i].DocNo,i+1,lDoc[i].score)
         
