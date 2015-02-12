e2743fd83550385b8b0b79f6310a764a9edb79a4
chenyanxiong <xiongchenyan@gmail.com>
2015-02-12 13:19:19 -0500

    add reader by separator line

---
+++ cxBase/SeparatorlineFileReader.py
@@ -0,0 +1,84 @@
+'''
+Created on Feb 12, 2015 11:45:07 AM
+@author: cx
+
+what I do:
+I read files with separator lines
+what's my input:
+
+what's my output:
+
+
+'''
+
+import gzip
+
+class SeparatorlineFileReaderC(object):
+    def Init(self):
+        self.InFile = ""
+        self.UseGzip = False
+        self.Spliter = '\t'
+        self.MaxLinePerFile = 100000
+        self.LastvCol = []
+        self.InName = ""
+        self.SeparatorPre = ''
+        self.CurrentSeparatorLine = ""
+    def __init__(self,SeparatorPre = 'trec'):
+        self.Init()
+        self.SeparatorPre = SeparatorPre
+        
+        
+    def open(self,InName,mode = 'r'):
+        self.InName = InName
+        if self.UseGzip:
+            self.InFile = gzip.open(InName,mode)
+        else:
+            self.InFile = open(InName,mode)
+            
+    def empty(self):
+        return "" == self.InName
+            
+    def close(self):
+        if not self.empty():
+            self.InFile.close()
+            self.InName = ""
+    
+    
+    def IsSeparatorLine(self,line):
+        if self.SeparatorPre == line[:len(self.SeparatorPre)]:
+            return True
+        return False    
+        
+    def ReadNextFile(self):
+        '''
+        start from a separator line, or not,
+        read until next separator line
+        '''
+        
+        lvCol = [] #one object's all cols
+        cnt = 0
+        for line in self.InFile:
+            line = line.strip()
+            if "" == line:
+                continue
+            if self.IsSeparatorLine(line):
+                self.CurrentSeparatorLine = line
+                if [] != lvCol:
+                    break
+            vCol = [self.CurrentSeparatorLine] + line.split(self.Spliter)
+            if [] == vCol:
+                continue
+            if cnt < self.MaxLinePerFile:
+                lvCol.append(vCol)
+            cnt += 1
+        return lvCol
+    
+    
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        lvCol = self.ReadNextFile()
+        if lvCol == []:
+            raise StopIteration
+        return lvCol    
