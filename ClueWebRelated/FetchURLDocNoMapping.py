b2d107d32523b8a66f614ed7e5672422405ca1d8
chenyanxiong <xiongchenyan@gmail.com>
2015-09-09 19:06:27 -0400

    get cw09 url doc no mapping

---
+++ ClueWebRelated/FetchURLDocNoMapping.py
@@ -0,0 +1,41 @@
+'''
+Created on Sep 9, 2015 5:42:51 PM
+@author: cx
+
+what I do:
+    I fetch url to doc no mapping
+what's my input:
+    a CW09 dir to work on
+what's my output:
+    URL->DocNo mapping of all warc files in the dir
+
+'''
+import warc,sys
+from cxBase.WalkDirectory import WalkDir
+
+def FetchOneDirURL(InDir,OutName):
+    lFName = WalkDir(InDir)
+    out = open(OutName,'w')
+    for InName in lFName:
+        In = warc.open(InName)
+        while True:
+            try:
+                record = In.read_record()
+            except (AssertionError, EOFError) as e:
+                break
+            if ('warc-trec-id' in record) & ('warc-target-uri' in record):
+                print >> out, record['warc-target-uri'] + '\t' + record('warc-trec-id')
+                
+        print '[%s] finished' %(InName)
+    out.close()
+    print 'dir [%s] finished' %(InDir)
+    return True
+
+
+if 3 != len(sys.argv):
+    print 'I will write all url-docno pairs'
+    print 'cw09 dir + out'
+    sys.exit()
+    
+FetchOneDirURL(sys.argv[1],sys.argv[2])
+
---
+++ ClueWebRelated/MulURLDocNoFetcher.py
@@ -0,0 +1,35 @@
+'''
+Created on Sep 9, 2015 5:49:15 PM
+@author: cx
+
+what I do:
+    I submit jobs for each dir of CW09 to get url-doc no pairs
+what's my input:
+    cw09 dir
+what's my output:
+    a dir of url-doc no mappings
+    done by qsub FetchURLDocNoMapping for each sub dir in input
+
+'''
+
+
+import subprocess
+import sys
+import ntpath
+
+if 3 != len(sys.argv):
+    print 'list of CW09 dir + out dir'
+    sys.exit()
+    
+    
+lCmd = ['qsub','FetchURLDocNoMapping.py']
+
+
+lFName = open(sys.argv[1]).read().splitlines()
+
+lOutName = [sys.argv[2] + '/' + ntpath.basename(fname.strip('/')) + '_URLDocNo' for fname in lFName]
+
+for InDir,OutName in zip(lFName,lOutName):
+    print subprocess.check_output(lCmd + [InDir,OutName])
+    
+    
