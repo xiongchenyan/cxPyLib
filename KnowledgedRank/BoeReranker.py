d3fa87badfc4962511a50effdc2a44fc337e558a
xiongchenyan <xiongchenyan@gmail.com>
2015-11-23 18:01:16 -0500

    add boe reranker and evaluate

---
+++ KnowledgedRank/BoeReranker.py
@@ -0,0 +1,175 @@
+'''
+Created on Nov 23, 2015 5:49:40 PM
+@author: cx
+
+what I do:
+    I rerank indri search results via doc's query entity score's
+what's my input:
+    query,
+    retrieved docs
+    query entities
+    doc's entity-score
+what's my output:
+    evaluation results
+
+'''
+
+
+
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+
+from cxBase.base import cxBaseC
+from cxBase.Conf import cxConfC
+import logging,json
+
+
+
+class BoeLmC(object):
+    def __init__(self):
+        self.Init()
+        
+    def Init(self):
+        self.MinWeight = -1000
+        return
+    
+    
+    def inference(self,ObjId,hDocEntity):
+        '''
+        return the log prob if p(ObjId| DocKg)
+        '''
+        
+        score = self.MinWeight
+        if ObjId in hDocEntity:
+            if hDocEntity[ObjId] != 0:
+                score = hDocEntity[ObjId]
+        return score
+
+
+class BoeLmRankerC(cxBaseC):
+    def Init(self):
+        cxBaseC.Init(self)
+        self.hQObj = {}
+        self.hDocKg = {}  #docNo->{obj:score}
+        self.Inferencer = BoeLmC()
+        
+        
+    def SetConf(self, ConfIn):
+        cxBaseC.SetConf(self, ConfIn)
+        DocEntityIn = self.conf.GetConf('dockgin')
+        QAnaInName = self.conf.GetConf('qanain')
+        self.LoadQObj(QAnaInName)
+        self.LoadDocObj(DocEntityIn)
+        self.SetInferencer()
+        
+    
+    @staticmethod
+    def ShowConf():
+        cxBaseC.ShowConf()
+        print 'dockgin\nqanain'
+        
+    
+    def SetInferencer(self):
+        self.Inferencer = BoeLmC()
+        return
+            
+    
+    def LoadQObj(self,QAnaInName):
+        for line in open(QAnaInName).read().splitlines():
+            vCol = line.strip().split('\t')
+            qid = vCol[0]
+            ObjId = vCol[2]
+            score = float(vCol[-1])
+            if not qid in self.hQObj:
+                self.hQObj[qid] = [[ObjId,score]]
+            else:
+                self.hQObj[qid].append([ObjId,score])
+                
+        logging.info('qobj loaded from [%s]',QAnaInName)
+        return True
+    
+    def LoadDocObj(self,DocEntityIn):
+        for line in open(DocEntityIn):
+            DocNo,EStr = line.strip().split('\t')
+            hDocEntity = json.loads(EStr)
+            self.hDocKg[DocNo] = hDocEntity
+        logging.info('doc kg loaded from [%s]',DocEntityIn)
+    
+    
+    def RankScoreForDoc(self,qid,doc):
+        if not doc.DocNo in self.hDocKg:
+            return self.Inferencer.MinWeight
+        
+        lQObj = self.hQObj[qid]
+        hDocEntity = self.hDocKg[doc.DocNo]
+
+        score = 0
+        for ObjId,weight in lQObj:
+            ObjScore = self.Inferencer.inference(ObjId, hDocEntity)
+            score += ObjScore * weight
+            logging.info('[%s] [%s] - [%s] obj score: %f',qid,doc.DocNo,ObjId,ObjScore)
+        
+        logging.info('[%s] [%s] ranking score: %f',qid,doc.DocNo,score)
+        return score
+    
+    def Rank(self,qid,query,lDoc):
+        if not qid in self.hQObj:
+            logging.warn('qid [%s] no ana obj, withdraw to given score',qid)
+            return [doc.DocNo for doc in lDoc]
+        
+        lScore = [self.RankScoreForDoc(qid, doc) for doc in lDoc]
+        lMid = zip(lDoc,lScore)
+        lDocNoScore = [[item[0].DocNo,item[1],item[0].score] for item in lMid]
+        #sort doc by two keys, if boe scores tie, use original ranking score
+        lDocNoScore.sort(key=lambda item: (item[1],item[2]), reverse = True)
+        
+        lRankRes = [item[0] for item in lDocNoScore]
+        return lRankRes
+    
+    
+    
+
+if __name__=='__main__':
+    import sys,os
+    from AdhocEva.RankerEvaluator import RankerEvaluatorC
+    if 2 != len(sys.argv):
+        print 'I evaluate Boe model '
+        print 'in\nout'
+        BoeLmRankerC.ShowConf()
+        RankerEvaluatorC.ShowConf()
+        
+        sys.exit()
+    
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+    ch.setFormatter(formatter)
+    
+    
+    
+    conf = cxConfC(sys.argv[1])   
+    QIn = conf.GetConf('in')
+    EvaOut = conf.GetConf('out')
+    
+    Ranker = BoeLmRankerC(sys.argv[1])
+    Evaluator = RankerEvaluatorC(sys.argv[1])
+    Evaluator.Evaluate(QIn, Ranker.Rank, EvaOut)
+     
+    
+        
+        
+        
+        
+        
+            
+            
+        
+        
+
+
+
+
---
+++ KnowledgedRank/__init__.py
@@ -0,0 +1,5 @@
+'''
+I evaluate the performance of ranking with knowledged doc representation
+
+E.g: simple BOE rerank model
+'''
