c2f269083a1b2b622599f1f16426a88673395e70
xiongchenyan <xiongchenyan@gmail.com>
2015-12-15 12:30:06 -0500

    try

--- KnowledgedRank/BoeReranker.py
+++ KnowledgedRank/BoeReranker.py
@@ -49,11 +49,11 @@ class BoeLmC(object):
             Z = math.log(Z)
         
         if ObjId in hDocEntity:
-            score = hDocEntity[ObjId]
-#             score = 1
+#             score = hDocEntity[ObjId]
+            score = 1
             if self.Normilize:
-                score += Z
-#                 score -= math.log(float(len(doc.lPosition)))
+#                 score += Z
+                score -= math.log(float(len(doc.lPosition)))
                 
         return score
 
