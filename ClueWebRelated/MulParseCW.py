6af59c81ae9fdd9642bf5f13b267d52eae0d55fd
xiongchenyan <xiongchenyan@gmail.com>
2015-12-19 20:03:36 -0500

    mul runner

---
+++ ClueWebRelated/MulParseCW.py
@@ -0,0 +1,52 @@
+'''
+Created on Dec 19, 2015 7:52:43 PM
+@author: cx
+
+what I do:
+    submit one job for each parser
+what's my input:
+    CW12 dir
+    out dir
+what's my output:
+    same organization, each file is parsed
+
+'''
+
+import subprocess
+import os
+import ntpath
+
+
+def WalkDir(InDir):
+    lFName = []
+    for dirname,dirnames,filenames in os.walk(InDir):
+        for filename in filenames:
+            lFName.append(dirname + "/" + filename)
+    return lFName
+
+
+def Process(InDir,OutDir):
+    lFName = WalkDir(InDir)
+    
+    lFName = [fname for fname in lFName if fname.endswith('warc.gz')]
+    
+    
+    lCmd = ['qsub','python','ParseClueWebDoc.py']
+    for fname in lFName:
+        OutName = OutDir + '/' + fname[len(InDir):]
+        MidDir = ntpath.dirname(OutName)
+        if not os.path.exists(MidDir):
+            os.makedirs(MidDir)
+        
+        print subprocess.check_output(lCmd + [fname,OutName])
+        
+        
+import sys
+
+if 3 != len(sys.argv):
+    print 'parse CW'
+    print 'CW InDir + outdir'
+    sys.exit()
+Process(sys.argv[1], sys.argv[2])
+        
+    
