5c79c5cb7b3286f1fb16ccb3537b2dafd1ac4a1d
xiongchenyan <xiongchenyan@gmail.com>
2016-01-06 15:36:26 -0500

    add qrel filter

---
+++ ClueWebRelated/RankingRelated/QRelFilterToCW09CatB.py
@@ -0,0 +1,42 @@
+'''
+Created on Jan 6, 2016 3:29:08 PM
+@author: cx
+
+what I do:
+    I filter qrel file to only keep cat b ones
+what's my input:
+    qrel
+what's my output:
+    qrel but only cat b ones are kept
+
+'''
+
+import sys
+
+if 3 != len(sys.argv):
+    print 'I filter qrel to cat b only'
+    print 'in qrel + out'
+
+lCatBMid = []
+for i in range(12):
+    lCatBMid.append('en%04d',i)
+    
+for i in range(4):
+    lCatBMid.append('enwp%02d',i)
+    
+    
+out = open(sys.argv[2],'w')
+sCatBMid = set(lCatBMid)
+for line in open(sys.argv[1]):
+    line = line.strip()
+    mid = line.strip('-')[1]
+    if mid in sCatBMid:
+        print >>out, line
+        
+        
+out.close()
+print 'finished'
+    
+    
+
+
---
+++ ClueWebRelated/RankingRelated/__init__.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 6, 2016 3:28:36 PM
+@author: cx
+
+what I do:
+    codes about ranking part data processing for CW09
+what's my input:
+
+what's my output:
+
+
+'''
