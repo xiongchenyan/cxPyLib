073b086fcc99c51fbc5622a2a62131e9d1797829
xiongchenyan <xiongchenyan@gmail.com>
2016-01-13 22:39:13 -0500

    encode clueweb

---
+++ ClueWebRelated/ClueWebWarcRelated/RawHtmlEncoding.py
@@ -0,0 +1,34 @@
+'''
+Created on Jan 13, 2016 10:36:54 PM
+@author: cx
+
+what I do:
+
+what's my input:
+
+what's my output:
+
+
+'''
+
+import sys,string
+
+
+if 3 != len(sys.argv):
+    print 'in + out (encode to ascii)'
+    sys.exit()
+
+out = open(sys.argv[2],'w')
+ErrCnt  = 0
+for cnt, text in enumerate(open(sys.argv[1])):
+    try:
+        text = text.strip().encode('ascii','ignore')
+        text = filter(lambda x: x in string.printable, text)
+        print >>out, text
+    except Exception as e:
+        ErrCnt += 1
+        
+        
+print 'finished [%d/%d]' %(ErrCnt,cnt)
+        
+        
